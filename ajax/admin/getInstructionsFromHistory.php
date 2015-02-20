5019ad63849116144b45cf04d512b9cc76f13d40
khaivngo <khai.vngo@gmail.com>
2015-02-20 13:34:26 +0100

    part of issue 57

--- ajax/admin/getInstructionsFromHistory.php
+++ ajax/admin/getInstructionsFromHistory.php
@@ -16,10 +16,11 @@ $stmt = $db->prepare("SELECT workPlace FROM workplacebelongs WHERE personId = :i
 $stmt->execute(array(':id' => $userId));	
 $res2 = $stmt->fetch();
 
+$option = $_POST['option']; //option might be 0 = access within admin panel or 1 = access within scientist panel
 
 //If access level is superuser all instructions in DB are fetched.
 
-if($res[0][0] == "Superuser")	{				//fetches all users
+if($res[0][0] == "Superuser" && $option == 0)	{				//fetches all users
 	try {
 		$stmt = $db->prepare("SELECT text FROM instruction WHERE text != 'NULL'");
 	
@@ -33,11 +34,11 @@ if($res[0][0] == "Superuser")	{				//fetches all users
 		ChromePhp::log($excpt->getMessage());
 	}
 }
-else if ($res[0][0]) == "Admin"	{		//Fetches all instructions from DB that belongs to user associated with institution of admin
+else if ($res[0][0] == "Admin" && $option == 0)	{		//Fetches all instructions from DB that belongs to user associated with institution of admin
 	$workPlace = $res2[0][0];
 	
 	try {
-		$stmt = $db->prepare("Select instruction.text from person JOIN workplacebelongs ON person.id = workplacebelongs.personId WHERE workplacebelongs.workPlace = '" . $workPlace . "' AND text != 'NULL' ");
+		$stmt = $db->prepare("Select instruction.text, instruction.id from person JOIN workplacebelongs ON person.id = workplacebelongs.personId WHERE workplacebelongs.workPlace = '" . $workPlace . "' AND text != 'NULL' ");
 	
 		$stmt->execute();	
 	
@@ -50,11 +51,11 @@ else if ($res[0][0]) == "Admin"	{		//Fetches all instructions from DB that belon
 	}
 	
 }
-else if ($res[0][0]) == "Scientist"	{ //Fetches all instructions belonging to current scientist.
+else if ($res[0][0] == "Scientist" || $option == 1)	{ //Fetches all instructions belonging to current scientist.
 	$workPlace = $res2[0][0];
 	
 	try {
-		$stmt = $db->prepare("Select instruction.text from person JOIN instruction ON person.id = instruction.personId WHERE instruction.personId = '" . $userId . "' AND text != 'NULL' ");
+		$stmt = $db->prepare("Select instruction.text, instruction.id from person JOIN instruction ON person.id = instruction.personId WHERE instruction.personId = '" . $userId . "' AND text != 'NULL' ");
 	
 		$stmt->execute();	
 	
@@ -66,6 +67,24 @@ else if ($res[0][0]) == "Scientist"	{ //Fetches all instructions belonging to cu
 		ChromePhp::log($excpt->getMessage());
 	}
 
+}
+else if($option == 3)    {
+    try {
+        $stmt = $db->prepare("SELECT DISTINCT experiment.title, experiment.id, experimentorder.instruction, instruction.text, instruction.id FROM experiment as origin, experimentqueue ".
+            " JOIN experiment ON experiment.id = experimentqueue.experiment " .
+            " JOIN experimentorder ON experimentqueue.id = experimentorder.experimentqueue " .
+            " JOIN instruction ON instruction.id = experimentorder.instruction " .
+            "  WHERE experiment.person = '" . $userId . "'");
+
+        $stmt->execute();
+
+        $res = $stmt->fetchAll();
+
+        echo json_encode($res);
+
+    } catch (PDOException $excpt) {
+        ChromePhp::log($excpt->getMessage());
+    }
 }
 
 ?>
--- ajax/observer/updateColourBlindness.php
+++ ajax/observer/updateColourBlindness.php
@@ -9,10 +9,10 @@ $userId = $_SESSION['user']['id'];              //fetching user id from session
 
 try {               //updates users colourblindness
     $stmt = $db->query("UPDATE person SET colourBlindFlag = '1' WHERE id = '" . $userId . "'");
+    $_SESSION['user']["colourBlindFlag"] = 1;
 
-    
 } catch (Exception $excpt) {
 //    ChromePhp::log($excpt->getMessage());
-    
+
 }
 ?>
