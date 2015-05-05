9cc3e04ffcad920e0d6db94fc3f9533450893d6c
khaivngo <khai.vngo@gmail.com>
2015-05-05 20:49:16 +0200

    Fixes #73

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 session_start();
 $db = new PDO(
         'mysql:host=127.0.0.1;' .
-        'dbname=db_quickeval;', 'root', 'mysql');
+        'dbname=db_quickeval;', 'root', '');
 
 $db->setAttribute(PDO::ATTR_ERRMODE, 
         PDO::ERRMODE_EXCEPTION);
