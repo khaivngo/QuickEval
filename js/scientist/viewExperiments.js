7c59841653d8534600408a96a8ce0670901d3aa9
mariuspedersen <marius.pedersen@hig.no>
2015-06-30 09:43:34 +0200

    Fixes error in z-score calculation for pair comparison
    Fixes an error in pair comparison calculation. The slope was calculated
    "wrong", and gave in some cases slightly wrong z-scores.

--- js/scientist/viewExperiments.js
+++ js/scientist/viewExperiments.js
@@ -1299,15 +1299,18 @@ function calculateSlope(y, x) {
     var sum_xx = 0;
     var sum_yy = 0;
 
-    for (var i = 0; i < y.length; i++) {
-
-        sum_x += x[i];
-        sum_y += y[i];
-        sum_xy += (x[i] * y[i]);
-        sum_xx += (x[i] * x[i]);
-        sum_yy += (y[i] * y[i]);
-    }
-
+	
+		for (var i = 0; i < y.length; i++) {
+			if(y[i]<3 && y[i] > -3)
+			{
+			sum_x += x[i];
+			sum_y += y[i];
+			sum_xy += (x[i] * y[i]);
+			sum_xx += (x[i] * x[i]);
+			sum_yy += (y[i] * y[i]);
+			}
+		}
+	
     lr['slope'] = (n * sum_xy - sum_x * sum_y) / (n * sum_xx - sum_x * sum_x);
     lr['intercept'] = (sum_y - lr.slope * sum_x) / n;
     lr['r2'] = Math.pow((n * sum_xy - sum_x * sum_y) / Math.sqrt((n * sum_xx - sum_x * sum_x) * (n * sum_yy - sum_y * sum_y)), 2);
