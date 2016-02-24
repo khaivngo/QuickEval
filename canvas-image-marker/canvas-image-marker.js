880336daa7879a4ef883f288ba0340ccf8b343a1
Robin Bekkevold <robin.vigdal.bekkevold@gmail.com>
2016-02-24 10:48:40 +0100

    Annotation when clicking on marked area.

--- canvas-image-marker/canvas-image-marker.js
+++ canvas-image-marker/canvas-image-marker.js
@@ -1,91 +1,129 @@
 (function($) {
-    $.fn.canvasAreaDraw = function(options) {
-        this.each(function(index, element) {
-            init.apply(element, [index, element, options]);
-        });
-    }
-
-    var init = function(index, canvasContainer, options) {
-        var settings;
-        var canvas, ctx, image;
-
-        var draw, mousedrag, stopdrag, move, resize, reset, undo;
-
-        var activePoint;
+    var initCanvasMarkerTool = function() {
+        var canvas = $('<canvas>');
+        var ctx = canvas[0].getContext('2d');
+        var annotation = $('.annotation');
+        var annotationText = $('#annotationText');
+        var canvasContainer = $('.canvas-container');
+        var image;
         var points = [];
         var savedShapes = [];
 
-        canvas = $('<canvas>');
-        ctx = canvas[0].getContext('2d');
-
-        image = new Image();
-        resize = function() {
-            canvas.attr('height', image.height).attr('width', image.width);
-        };
-        $(image).load(resize);
-        image.src = $('.canvas-container').attr('data-image-url');
-        if (image.loaded) resize();
-        canvas.css({ background: 'url(' + image.src + ')' });
-
         $(document).ready( function() {
+            setCanvasImage();
+
             $(canvasContainer).append(canvas); /* append the resized canvas to the DOM */
+            canvas.on('mousedown', startdrag);
+            canvas.on('mouseup', stopdrag);
+            canvas.on('mousedown', click);
 
             $('#undo').on('click', undo);
             $('#reset').on('click', reset);
+            $('#saveAnnotation').on('click', saveAnnotation);
+            $('#closeAnnotation').on('click', closeAnnotation);
 
-            canvas.on('mousedown', startdrag);
-            canvas.on('mouseup', stopdrag);
-
-            $(document).keydown(function(e) {
-                if (e.keyCode == 27) undo();
-            });
+            // params: element which will be moved, element which is dragable
+            makeDraggable( ".annotation", "#annotationButtons" );
         });
 
-        reset = function() {
-            points = [];
-            savedShapes = [];
-            draw();
+        var setCanvasImage = function() {
+            image = new Image();
+
+            resize = function() {
+                canvas.attr('height', image.height).attr('width', image.width);
+            };
+
+            $(image).load(resize);
+            image.src = canvasContainer.attr('data-image-url');
+
+            if (image.loaded)
+                resize();
+
+            canvas.css({ background: 'url(' + image.src + ')' });
         };
 
-        stopdrag = function() {
-            $(this).off('mousemove');
+        /**
+         * Get the value from the data-id attribute of the annotation container,
+         * and update the savedShape at the corresponding array index.
+         */
+        var saveAnnotation = function(e) {
+            e.preventDefault();
 
-            saveShape();
-            console.log(savedShapes[0].points.toString());
+            var shapeID = annotation.attr('data-id');
+            var annoText = $.trim(annotationText.val());
+
+            /* get the annotation text and update the saved shape with that id */
+            savedShapes[shapeID].annotation = annoText;
+
+            closeAnnotation(e);
+        };
+
+        var closeAnnotation = function(e) {
+            e.preventDefault();
+            annotation.css('display', 'none');
+        };
+
+        var click = function(e) {
+            var mouseX  = e.offsetX;
+            var mouseY  = e.offsetY;
+
+            ctx.lineWidth = 2;
+
+            for (var k = 0; k < savedShapes.length; k++) {
+                ctx.beginPath();
+                ctx.moveTo(savedShapes[k].points.x, savedShapes[k].points.y);
+
+                for (var d = 0; d < savedShapes[k].points.length; d++) {
+                    ctx.lineTo(savedShapes[k].points[d].x, savedShapes[k].points[d].y);
+                }
+
+                if (ctx.isPointInPath(mouseX, mouseY)) {
+                    annotation.css({
+                        'display': 'block',
+                        'top': (mouseY - 140) + "px",
+                        'left': (mouseX - 125) + "px"
+                    });
+                    annotation.attr('data-id', k);
+                    annotationText.val(savedShapes[k].annotation);
+
+                    return; /* we found the clicked polygon, no need to loop through the rest */
+                }
+                ctx.closePath();
+            }
+        };
+
+        var stopdrag = function(e) {
+            $(this).off('mousemove');
+            /* we're done drawing, save the shape */
+            saveShape(e);
         };
 
         var startdrag = function() {
             $(this).on('mousemove', mousedrag);
         };
 
-
         /**
-         * Set distance of each point.
-         * Effectively decreasing or increasing precision of the marking.
+         * Calls the drawing function each time the mouse
+         * has been dragged a certain distance.
          *
          * @return {boolean} false
          */
-        mousedrag = function(e) {
+        var mousedrag = function(e) {
             e.preventDefault();
 
-            var x;
-            var y;
             var dis;
-            var insertAt = points.length;
+            var x = e.offsetX;
+            var y = e.offsetY;
 
-            x = e.offsetX;
-            y = e.offsetY;
-
-            for (var i = 0; i < points.length; i+=2) {
-                dis = Math.sqrt(Math.pow(x - points[i], 2) + Math.pow(y - points[i+1], 2));
-                if ( dis < 8 ) {
-                    activePoint = i;
+            for (var i = 0; i < points.length; i++) {
+                dis = Math.sqrt(Math.pow(x - points[i].x, 2) + Math.pow(y - points[i].y, 2));
+                if ( dis < 6 ) {
+                    // return if we do not have enough distance
                     return false;
                 }
             }
 
-            points.splice(insertAt, 0, Math.round(x), Math.round(y));
-            activePoint = insertAt;
+            points.push({ x: Math.round(x), y: Math.round(y) });
 
             draw();
 
@@ -98,46 +136,49 @@
          *
          * @return void
          */
-        draw = function() {
+        var draw = function() {
             /* clear the canvas each time */
             ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-            /* if we have any saved shapes, draw them first */
+            drawSavedShapes();
+
+            if (points.length > 0) {
+                ctx.fillStyle = 'rgba(0, 0, 100, 0.3)';
+                ctx.strokeStyle = 'rgb(0, 0, 100)';
+                ctx.lineWidth = 2;
+
+                ctx.beginPath();
+                ctx.moveTo(points[0].x, points[0].y);
+
+                for (var i = 0; i < points.length; i++) {
+                    if (points.length > 1) {
+                        ctx.lineTo(points[i].x, points[i].y);
+                    }
+                }
+
+                ctx.fill();
+                ctx.stroke();
+            }
+        };
+
+        var drawSavedShapes = function() {
             if (savedShapes.length > 0) {
-                ctx.fillStyle = 'rgb(255, 255, 255)';
+                ctx.fillStyle = 'rgba(0, 100, 0, 0.3)';
                 ctx.strokeStyle = 'rgb(116, 175, 91)';
                 ctx.lineWidth = 2;
 
                 for (var k = 0; k < savedShapes.length; k++) {
                     ctx.beginPath();
-                    ctx.moveTo(savedShapes[k].points[0], savedShapes[k].points[1]);
-                    for (var d = 0; d < savedShapes[k].points.length; d += 2) {
-                        if (savedShapes[k].points.length > 2 && d > 1) {
-                            ctx.lineTo(savedShapes[k].points[d], savedShapes[k].points[d + 1]);
-                        }
+                    ctx.moveTo(savedShapes[k].points.x, savedShapes[k].points.y);
+
+                    for (var d = 0; d < savedShapes[k].points.length; d++) {
+                        ctx.lineTo(savedShapes[k].points[d].x, savedShapes[k].points[d].y);
                     }
                     ctx.closePath();
-                    ctx.fillStyle = 'rgba(0, 100, 0, 0.3)';
                     ctx.fill();
                     ctx.stroke();
                 }
             }
-
-            /* draw current path (shape) */
-            ctx.fillStyle = 'rgb(255, 255, 255)';
-            ctx.strokeStyle = 'rgb(249, 49, 49)';
-            ctx.lineWidth = 2;
-
-            ctx.beginPath();
-            ctx.moveTo(points[0], points[1]);
-            for (var i = 0; i < points.length; i+=2) {
-                if (points.length > 2 && i > 1) {
-                    ctx.lineTo(points[i], points[i+1]);
-                }
-            }
-            ctx.fillStyle = 'rgba(250, 250, 250, 0.3)';
-            ctx.fill();
-            ctx.stroke();
         };
 
         /**
@@ -146,13 +187,15 @@
          *
          * @return void
          */
-        saveShape = function() {
-            /* save all the x and y coordinates as well as any comment */
-            savedShapes.push({
-                points: points,
-                comment: "Hello from the other side"
-            });
-            points = [];
+        var saveShape = function(e) {
+            // only save the shape if we have atleast 3 points
+            if (points.length > 2) {
+                // save all the x and y coordinates as well as any comment
+                savedShapes.push({ points: points, annotation: "" });
+            }
+
+            points = []; /* remove the current shape now that it's saved */
+
             draw();
         };
 
@@ -161,7 +204,7 @@
          *
          * @return void
          */
-        undo = function() {
+        var undo = function() {
             if (points.length > 0)
                 points = [];
             else
@@ -170,12 +213,41 @@
             draw();
         };
 
+        var reset = function() {
+            points = [];
+            savedShapes = [];
+
+            draw();
+        };
+
+        /**
+         * Make any element draggable.
+         *
+         * @param {string} id or class name of html element which is grabable
+         * @param {string} id or class name of html element to be moved
+         */
+        var makeDraggable = function(grabable, moveable) {
+            $(grabable).on('mousedown', moveable, function() {
+                $(grabable).addClass('draggable').parents().on('mousemove', function(e) {
+                    e.preventDefault();
+                    $('.draggable').offset({
+                        top: e.pageY - $('.draggable').outerHeight() / 2,
+                        left: e.pageX - $('.draggable').outerWidth() / 2
+                    }).on('mouseup', function() {
+                        $(grabable).removeClass('draggable');
+                    });
+                });
+            }).on('mouseup', function() {
+                $('.draggable').removeClass('draggable');
+            });
+        };
+
     }; // end of init()
 
 
-    /* Bootstrap the canvas. Let's go! */
+    /* Let's go. */
     $(document).ready(function() {
-        $('.canvas-container[data-image-url]').canvasAreaDraw();
+        initCanvasMarkerTool();
     });
 
 })(jQuery);
