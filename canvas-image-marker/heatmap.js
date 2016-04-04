2ea472cf9f49155919091c992696366449b6e974
Henrik Oddløkken <heenriko@gmail.com>
2016-04-04 16:12:49 +0200

    finished with heatmap legend. Added a method for changing the slider with input values.

--- canvas-image-marker/heatmap.js
+++ canvas-image-marker/heatmap.js
@@ -11,22 +11,29 @@ var image = new Object();
 image.width = 800;
 image.height = 800;
 
-var canvasHeatmapLegend = 40;
+var heatmapLegend = new Object();
+heatmapLegend.height = 60;							// Extended value for heatmap legend scale in canvas.
+heatmapLegend.square = heatmapLegend.height / 2;	// Height/width for each square box in the heatmap legend. 
+heatmapLegend.marginTop = 5;						// Remember to consider the heatmapLegend.height value when assigning new value.
+heatmapLegend.marginLeft = 75;						// Remember to consider the image.width value when assigning new value.
+heatmapLegend.paddingLeft = 4;						// Padding left for each square box.				
 
+
+// Canvas base image:
+var imageCanvas = $('<canvas>');
+var imageCtx = imageCanvas[0].getContext('2d');
+
+// Canvas where the drawing will take place:
 var matrixCanvas = $('<canvas>');
 var matrixCtx = matrixCanvas[0].getContext('2d');
 
-matrixCtx.fillstyle = "#000";
+// Merged canvas from image and marking:
+var mergedCanvas = $('<canvas>');
+var mergedCtx = mergedCanvas[0].getContext('2d');
 
-matrixCanvas.attr('height', image.height + canvasHeatmapLegend).attr('width', image.width).attr('id','heatmapCanvasImage');
-
-// Believe this is temporally code: 
-/* matrixCanvas.css
-({
-	'background':'url(img/final13.bmp)', 
-	'background-repeat':'no-repeat',
-	'background-color':'#333'
-}); */
+imageCanvas.attr('height', image.height + heatmapLegend.height).attr('width', image.width).attr('id','heatmapCanvasImage');
+matrixCanvas.attr('height', image.height + heatmapLegend.height).attr('width', image.width).attr('id','heatmapCanvasMatrix');
+mergedCanvas.attr('height', image.height + heatmapLegend.height).attr('width', image.width).attr('id','heatmapCanvasMerged');
 
 setImage();
 
@@ -36,15 +43,17 @@ function setImage()
 	base_image.src = 'img/final13.bmp';
 	base_image.onload = function()
 	{
-		
-		matrixCtx.drawImage(base_image, 0, 0);
+		imageCtx.drawImage(base_image, 0, 0);
 	}
 }
 
 
 $(document).ready(function()
 {	
+	$('#heatmapCanvasContainer').append(imageCanvas);
 	$('#heatmapCanvasContainer').append(matrixCanvas);
+	$('#heatmapCanvasContainer').append(mergedCanvas);
+	
 	$.ajax
 	({
 		url: 'fetchShapesFromDb.php'
@@ -64,7 +73,10 @@ $(document).ready(function()
 	});
 	
 	// Generate heatmap button.
-	$('#genHeatmap').on('click',function(){calcPolygonPoints();});
+	$('#genHeatmap').on('click',function()
+	{
+		calcPolygonPoints();
+	});
 	
 	/*------*/
 	
@@ -73,7 +85,7 @@ $(document).ready(function()
 	 *	When the user changes the Sliders range, either hue or saturation.
 	 *  @return {void}.
 	 */
-	$('input[type=range]').on('input',function(event)
+	$('#heatmapPanel li input[type=range]').on('input',function(event)
 	{
 		var hue = $('#hueLevel').val();		// Get the current hue level.
 		var sat = $('#satLevel').val();		// Get the current saturation level.
@@ -81,6 +93,20 @@ $(document).ready(function()
 		$(this).parent().parent().find('.sizeNumber').val( $(this).val() ); // Change current label text value for this slider.
 		
 		setSliderColor(hue, sat);			
+		
+		if( $('#liveGen[type=checkbox]').is(':checked') )
+			calcPolygonPoints();
+	});
+	
+	$('#heatmapPanel li input[type=number]').on('input',function(event)
+	{
+		var hue = $('#hueSection input[type=number]').val();
+		var sat = $('#satSection input[type=number]').val();
+		
+		$('#hueLevel').val(hue);
+		$('#satLevel').val(sat);
+		
+		setSliderColor(hue, sat);
 		
 		if( $('#liveGen[type=checkbox]').is(':checked') )
 			calcPolygonPoints();
@@ -119,14 +145,10 @@ $(document).ready(function()
 	});
 	
 	
-	/* $('#downloadImage').on('click', function() 
+	document.getElementById('downloadImage').addEventListener('click', function()
 	{
-		downloadCanvas($(this), 'heatmapCanvasImage', 'test.png');
-	}, false); */
-	
-	document.getElementById('downloadImage').addEventListener('click', function() {
-    downloadCanvas(this, 'heatmapCanvasImage', 'test.png');
-}, false);
+		downloadCanvas(this, 'heatmapCanvasMerged', 'test.png');
+	}, false);
 
 });
 
@@ -134,8 +156,17 @@ $(document).ready(function()
 
 function downloadCanvas(link, canvasId, filename)
 {
+	var imageCanvasTemp = document.getElementById('heatmapCanvasImage');
+	var matrixCanvasTemp = document.getElementById('heatmapCanvasMatrix');
+	
+	mergedCtx.drawImage(imageCanvasTemp, 0, 0);
+	mergedCtx.drawImage(matrixCanvasTemp, 0, 0);
+	
 	link.href = document.getElementById(canvasId).toDataURL(); 
 	link.download = filename;
+	
+	// Reset mergedCtx for further manipulation
+	mergedCtx.clearRect(0, 0, image.width, image.height + heatmapLegend.height);
 }
 
 function setSliderColor(hue, sat)
@@ -193,18 +224,14 @@ function createMatrix(data)
 function hueScale(valPerc, sat)
 {
 	var hue = HUE_LOW - (HUE_LOW * valPerc);
-	
 	var color = 'hsl(' + hue + ',' + sat + '%,50%)';
-
 	return color;
 }
 
 function grayScale(valPerc, hue, sat)
 {
 	var light = valPerc * 100;
-
 	var color = 'hsl(' + hue + ',' + sat + '%,' + light + '%)';
-
 	return color;
 }
 
@@ -237,36 +264,36 @@ function heatmapColor(cur, max, scaleType, hue, sat, reverse)
 function renderHeatmapLegend(scaleType, maxVal, hue, sat, reverse)
 {
 	var colorStep = 0;
-	
+	var deltaPadding = 0;
+	var index = 0;
 	var range = 10;
 	
 	if(maxVal < 10)
 		range = maxVal;
-
-	/* var htmlScale = "";
-	$('#heatmapLegend').remove(); */
-
-	var delta = 0;
+	
+	var x = 0;
+	var y = image.height + heatmapLegend.marginTop;
+	
+	
 	
 	switch (scaleType)
 	{
 		case 0:
 			colorStep = 100 / (range-1);
-
-			/* htmlScale += '<div id = "heatmapLegend">'; */
-			
 			
 			if(!reverse)
 			{	
 				for(var i = 0; i < range; i++)
 				{
 					var color = 'hsl(' + hue + ',' + sat + '%,' + colorStep * i + '%)';
-					/* htmlScale += '<div class = "scaleItem" style = "background-color:'+color+'"></div>'; */
-					
 					matrixCtx.fillStyle = color;
-					matrixCtx.fillRect( 30 * i + delta + 50, 805, 30, 30 );
 					
-					delta += 4;
+					x = heatmapLegend.square * index + deltaPadding + heatmapLegend.marginLeft;
+					
+					matrixCtx.fillRect( x, y, heatmapLegend.square, heatmapLegend.square );
+
+					deltaPadding += heatmapLegend.paddingLeft;
+					index ++;
 				}
 			}
 			else
@@ -274,36 +301,34 @@ function renderHeatmapLegend(scaleType, maxVal, hue, sat, reverse)
 				for(var i = range-1; i >= 0; i--)
 				{
 					var color = 'hsl(' + hue + ',' + sat + '%,' + colorStep * i + '%)';
-					/* htmlScale += '<div class = "scaleItem" style = "background-color:'+color+'"></div>'; */
-					
 					matrixCtx.fillStyle = color;
-					matrixCtx.fillRect( 30 * i + delta + 50, 805, 30, 30 );
 					
-					delta += 4;
+					x = heatmapLegend.square * index + deltaPadding + heatmapLegend.marginLeft;
 					
+					matrixCtx.fillRect( x, y, heatmapLegend.square, heatmapLegend.square );
+
+					deltaPadding += heatmapLegend.paddingLeft;
+					index ++;
 				}
 			}
-
-			/* htmlScale += '</div>'
-			$('#heatmap-panel-container').prepend(htmlScale); */
 			break;
 
 		case 1:
 			colorStep = HUE_LOW / (range-1);
-
-			htmlScale += '<div id = "heatmapLegend">';
 			
 			if(!reverse)
 			{	
 				for(var i = range-1; i >= 0; i--)
 				{
 					var color = 'hsl(' + colorStep * i + ',' + sat + '%, 50%)';
-					//htmlScale += '<div class = "scaleItem" style = "background-color:'+color+'"></div>';
-					
 					matrixCtx.fillStyle = color;
-					matrixCtx.fillRect( 30 * i + delta + 50, 805, 30, 30 );
 					
-					delta += 4;
+					x = heatmapLegend.square * index + deltaPadding + heatmapLegend.marginLeft;
+					
+					matrixCtx.fillRect( x, y, heatmapLegend.square, heatmapLegend.square );
+
+					deltaPadding += heatmapLegend.paddingLeft;
+					index ++;
 				}	
 			}
 			else
@@ -311,19 +336,37 @@ function renderHeatmapLegend(scaleType, maxVal, hue, sat, reverse)
 				for(var i = 0; i < range; i++)
 				{
 					var color = 'hsl(' + colorStep * i + ',' + sat + '%, 50%)';
-					//htmlScale += '<div class = "scaleItem" style = "background-color:'+color+'"></div>';
-					
 					matrixCtx.fillStyle = color;
-					matrixCtx.fillRect( 30 * i + delta + 50, 805, 30, 30 );
 					
-					delta += 4;
+					x = heatmapLegend.square * index + deltaPadding + heatmapLegend.marginLeft;
+					
+					matrixCtx.fillRect( x, y, heatmapLegend.square, heatmapLegend.square );
+
+					deltaPadding += heatmapLegend.paddingLeft;
+					index ++;
 				}
 			}
-
-			//htmlScale += '</div>'
-			//$('#heatmap-panel-container').prepend(htmlScale);
 			break;
 	}
+	
+	mergedCtx.fillstyle = "#000";
+	mergedCtx.font = "14px Arial";
+	
+	var textPadding = 5;
+	
+	var textPaddingleft = 0;
+	var textPosX_MAX = 0;
+	var textPosY = y + heatmapLegend.square;
+	
+	for(var i = 0; i < range; i++)
+	{
+		textPaddingleft += heatmapLegend.paddingLeft;
+	}
+	
+	textPosX_MAX = range * heatmapLegend.square + textPaddingleft + heatmapLegend.marginLeft + textPadding;
+	
+	mergedCtx.fillText("MIN 0", textPadding, textPosY);						// Set label for minimum value.
+	mergedCtx.fillText('MAX(' + maxVal + ')',textPosX_MAX + 20, textPosY );	// Set label for maximum value.
 }
 
 /**
@@ -382,6 +425,9 @@ function drawMatrixCanvas(data, hue, sat, scaleType, reverse)
 function calcPolygonPoints()
 {
 	console.log('number of shapes: ' + savedShapes.length);
+	//setImage();
+	matrixCtx.clearRect(0, 0, image.width, image.height + heatmapLegend.height);
+	mergedCtx.clearRect(0, 0, image.width, image.height + heatmapLegend.height);
 	
 	if(savedShapes.length > 0 )
 	{
