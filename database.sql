47bff19059f50c981ed8ba9c0fb2b2ae435cff73
disastersystem <robin.vigdal.bekkevold@gmail.com>
2017-01-13 15:06:01 +0100

    Merged bachelor_spring_2016. Run DB_UPDATES.SQL to update the database, or run the complete database.sql file.

--- database.sql
+++ database.sql
@@ -33,7 +33,7 @@ CREATE TABLE `artifactmark` (
   `marked_pixels` longtext NOT NULL,
   `remark` text NOT NULL,
   `person` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dataark for tabell `artifactmark`
@@ -1424,7 +1424,7 @@ INSERT INTO `experimenttype` (`id`, `name`, `type`, `description`) VALUES
 (2, 'Paired comparison', 'pair', 'Pick one'),
 (3, 'Category judgement', 'category', 'Place images into categories'),
 (4, 'Artifact marking', 'artifact', 'Let observers draw marks on interesting spots on images'),
-(5, 'Triplet comparison', 'triplet', 'Pick one image out of three');
+(5, 'Triplet comparison', 'triplet', 'Rate three images based in perceived quality');
 
 -- --------------------------------------------------------
 
